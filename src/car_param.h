cf2555c1417e5014cd18f1f6bdb54eab098fe780
Fukuda <61715611+fukuda39@users.noreply.github.com>
2024-05-08 18:09:46 +0900

    水温・油温・ブースト圧の表示画面を追加

--- src/car_param.h
+++ src/car_param.h
@@ -28,3 +28,8 @@ extern int dpf_reg_count;
 extern int dpf_reg_dist;
 extern int dpf_reg_status;
 extern float car_outside_temperature;
+extern float engine_coolant_temp;
+extern float engine_oil_temp;
+extern uint8_t manifold_pressure;
+extern uint8_t abs_baro_pressure;
+extern float boost_pressure;
